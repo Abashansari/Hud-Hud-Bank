1540bd9b0d4cc3d0584c2bcc4e2b43654a9f9fec
Abash Ansari <ansariabash2004@gmail.com>
2025-08-10 08:46:48 -0400

    created a Sign In page

---
+++ src/components/SignIn.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function SignIn() {
+    return (
+        <div>
+            <form>
+                <div className="mb-3">
+                    <label for="exampleInputEmail1" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div className="mb-3">
+                    <label for="exampleInputPassword1" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1" />
+                </div>
+                <button type="submit" className="btn btn-primary">Sign in</button>
+
+                <div className="text-center mt-3">
+                    <a href="/register" className="text-decoration-none">Don’t have an account? <u>Sign Up</u>
+                    </a>
+                </div>
+            </form>
+        </div>
+    )
+}
