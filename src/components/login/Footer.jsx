fdcd01bd09c5c9cfc1ca76df35c752c27c715fee
Abash Ansari <ansariabash2004@gmail.com>
2025-08-15 01:22:03 -0400

    add a class for footer & hr tag , also imported a class file

---
+++ src/components/login/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../../App.css'
+
+export default function LoginFooter() {
+    return (
+        <>
+        <div className='container mt-5 mb-5'>
+            <hr className='dashed-hr' />
+        </div>
+            <footer className="d-flex justify-content-between align-items-center py-3 px-4 text-muted">
+                <div>
+                    &copy; 2025 Hud Hud Bank. All rights reserved.
+                </div>
+                <div>
+                    Powered by Abash
+                </div>
+            </footer>
+        </>
+    )
+}
