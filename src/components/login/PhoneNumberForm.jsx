df19b888ddbace7eff47a623305904e9bca5a56e
Abash Ansari <ansariabash2004@gmail.com>
2025-08-15 02:08:30 -0400

    replaced class with className

--- src/components/login/PhoneNumberForm.jsx
+++ src/components/login/PhoneNumberForm.jsx
@@ -4,9 +4,9 @@ export default function PhoneNumberForm() {
   return (
     <form>
         <pre>Registered Mobile Number</pre>
-        <input class="input" type="number" placeholder="Enter your Phone Number :" />
+        <input className="input" type="number" placeholder="Enter your Phone Number :" />
         <p>we will send you an OTP on this mobile number</p>
-        <button class="button is-medium is-fullwidth">Get OTP</button>
+        <button className="button is-medium is-fullwidth">Get OTP</button>
     </form>
   )
 }
