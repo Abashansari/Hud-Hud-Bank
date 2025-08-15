cbb9168241b993fd7da831db8c9b0d193e5319ce
Abash Ansari <ansariabash2004@gmail.com>
2025-08-15 00:26:27 -0400

    update placeholder

--- src/components/login/PhoneNumberForm.jsx
+++ src/components/login/PhoneNumberForm.jsx
@@ -4,7 +4,7 @@ export default function PhoneNumberForm() {
   return (
     <form>
         <pre>Registered Mobile Number</pre>
-        <input class="input" type="number" placeholder="Text input" />
+        <input class="input" type="number" placeholder="Enter your Phone Number :" />
         <p>we will send you an OTP on this mobile number</p>
         <button class="button is-medium is-fullwidth">Get OTP</button>
     </form>
