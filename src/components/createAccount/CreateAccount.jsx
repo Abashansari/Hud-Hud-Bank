2201b9905f258f7af59cc7ef346e4039613e87e2
Abash Ansari <ansariabash2004@gmail.com>
2025-08-15 03:08:57 -0400

    imported  Navbar & Advertisement components

---
+++ src/components/createAccount/CreateAccount.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginNavber from '../login/LoginNavber'
+import Advertisement from './Advertisement'
+
+export default function CreateAccount() {
+  return (
+    <div className='container'>
+        <LoginNavber />
+        <Advertisement />
+    </div>
+  )
+}
