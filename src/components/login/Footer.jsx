e6da911c588785f8caab3faca0dfc08c4a395151
Abash Ansari <ansariabash2004@gmail.com>
2025-08-16 04:19:17 -0400

    added a strong tag & some class-container

--- src/components/login/Footer.jsx
+++ src/components/login/Footer.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import '../../App.css'
+import React from 'react';
+import '../../App.css';
 
 export default function LoginFooter() {
-    return (
-        <>
-        <div className='container mt-5 mb-5'>
-            <hr className='dashed-hr' />
+  return (
+    <>
+      <div className="container my-5">
+        <hr className="custom-hr" />
+      </div>
+      <footer className="container d-flex justify-content-between align-items-center py-3 px-4 text-white bg-black rounded-bottom">
+        <div>
+          &copy; 2025 Hud Hud Bank. All rights reserved.
         </div>
-            <footer className="d-flex justify-content-between align-items-center py-3 px-4 text-muted">
-                <div>
-                    &copy; 2025 Hud Hud Bank. All rights reserved.
-                </div>
-                <div>
-                    Powered by Abash
-                </div>
-            </footer>
-        </>
-    )
+        <div>
+          Powered by <strong>Abash</strong>
+        </div>
+      </footer>
+    </>
+  );
 }
