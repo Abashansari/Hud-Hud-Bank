91c2ae6359d5e6d2a6c1a5b14d19bd7e36d36562
Abash Ansari <ansariabash2004@gmail.com>
2025-08-17 03:41:50 -0400

    added title

--- src/components/transaction/MobileNumber.jsx
+++ src/components/transaction/MobileNumber.jsx
@@ -4,7 +4,8 @@ import '../../App.css'
 export default function MobileNumber() {
   return (
     <form className="fund-transfer-form p-4 mx-auto my-5 rounded shadow-sm" style={{ maxWidth: '500px' }}>
-
+         <h2 className="mb-4 text-center">Send Money</h2>
+         
       <div className="mb-3">
         <label htmlFor="mobileNumber" className="form-label">Enter Mobile Number</label>
         <input type="text" className="form-control" id="mobileNumber" inputMode="numeric" pattern="\d{10}" maxLength="10" placeholder="Pay Anyone" aria-describedby="numberHelp" required/>
