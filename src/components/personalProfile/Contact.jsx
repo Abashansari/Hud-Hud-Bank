51d30daacc6b3441580023c2bc0875f8c1dd8808
Abash Ansari <ansariabash2004@gmail.com>
2025-08-17 04:25:41 -0400

    added a card

--- src/components/personalProfile/Contact.jsx
+++ src/components/personalProfile/Contact.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div>This Contact page</div>
+    <div className="card" style={{ width: "18rem" }}>
+      <div className="card-header mb-3">Contact</div>
+
+      <ul className="list-group list-group-flush">
+        <li className="list-group-item">Email ID : ansari4004@gmail.com</li>
+        <li className="list-group-item">Mobile : 8167259866</li>
+      </ul>
+
+    </div>
   )
 }
