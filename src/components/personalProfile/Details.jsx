8fcf88dca4ecfc177406453cd30e87511d09fb9f
Abash Ansari <ansariabash2004@gmail.com>
2025-08-17 04:40:29 -0400

    added a card

--- src/components/personalProfile/Details.jsx
+++ src/components/personalProfile/Details.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function Details() {
   return (
-    <div>This is Details page</div>
+    <div className='container'>
+      <div className="card w-50">
+        <div className="card-body">
+          <h5 className="card-title">Abash Ansari</h5>
+          <p className="card-text">Customer ID XXXXXXXXXX</p>
+          <p className="card-text">Date of Birth 21/11/2004</p>
+          <p className="card-text">Gender Male</p>
+          <p className="card-text">TPIN XXXXXXXXXX</p>
+        </div>
+      </div>
+    </div>
   )
 }
--- src/components/personalProfile/MyQR.jsx
+++ src/components/personalProfile/MyQR.jsx
@@ -1,7 +1,20 @@
+import { QrCode } from '@mui/icons-material'
 import React from 'react'
 
 export default function MyQR() {
   return (
-    <div>This is MyQR</div>
+    <div className='container'>
+      <div className="card text-center">
+        <div className="card-header">
+          My QR code
+        </div>
+        <div className="card-body">
+          <h5 className="card-title">Abash Ansari</h5>
+          {/* <img src={QrCode} alt='My QR code' /> */}
+          <p className="card-text">Scan to pay with any App </p>
+        </div>
+        <div className="card-footer text-body-secondary">Hud Hud Bank</div>
+      </div>
+    </div>
   )
 }
