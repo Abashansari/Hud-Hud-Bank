dd58aa761533df8793299e0eaf0123438d9ac7df
Abash Ansari <ansariabash2004@gmail.com>
2025-08-17 04:41:28 -0400

    imported Details,Contact and MyQR components

--- src/components/personalProfile/PersonalProfile.jsx
+++ src/components/personalProfile/PersonalProfile.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import Details from './Details'
+import Contact from './Contact'
+import MyQR from './MyQR'
 
 export default function PersonalProfile() {
   return (
-    <div>This is PersonalProfile page</div>
+    <div className='container'>
+      <Details />
+      <Contact />
+      <MyQR />
+    </div>
   )
 }
