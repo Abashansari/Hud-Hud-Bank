827d88eb2bd7235c678d34123960728839189e64
Abash Ansari <ansariabash2004@gmail.com>
2025-08-17 04:05:40 -0400

    created a components

---
+++ src/components/personalProfile/MyQR.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MyQR() {
+  return (
+    <div>This is MyQR</div>
+  )
+}
---
+++ src/components/personalProfile/PersonalProfile.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function PersonalProfile() {
+  return (
+    <div>This is PersonalProfile page</div>
+  )
+}
