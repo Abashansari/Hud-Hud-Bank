5823397a4dec0ad20e3f462e0e2c457b7756cf48
Abash Ansari <ansariabash2004@gmail.com>
2025-08-24 03:17:02 -0400

    added a buttons

---
+++ src/components/dashboardOverview/TransactionHistory.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function TransactionHistory() {
+    return (
+        <div className='container d-flex justify-content-between align-item-center mt-5'>
+            <button className="button is-black">view Transaction</button>
+            <button className="button is-black">download Transaction Slip</button>
+        </div>
+    )
+}
