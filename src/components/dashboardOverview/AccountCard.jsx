b5c782d983cf913dfeb639834bc753555f906197
Abash Ansari <ansariabash2004@gmail.com>
2025-08-24 01:43:57 -0400

    added a title outside of card

--- src/components/dashboardOverview/AccountCard.jsx
+++ src/components/dashboardOverview/AccountCard.jsx
@@ -2,15 +2,21 @@ import React from 'react'
 
 export default function AccountCard() {
     return (
-        <div className="card w-50">
-            <div className="card-body">
-                <div className='container d-flex justify-content-between align-items-center'>
-                    <h5 className="card-title">My Account</h5>
-                    <a href="#" className="btn btn-primary"><img src='...' alt='Bank Logo' /></a>
+        <div>
+            <h4 className="mb-3">Account Overview</h4>
+
+            <div className="card w-50">
+                <div className="card-body">
+                    <div className='container d-flex justify-content-between align-items-center'>
+                        <h5 className="card-title">My Account</h5>
+                        <a href="#" className="btn btn-primary">
+                            <img src='...' alt='Bank Logo' />
+                        </a>
+                    </div>
+                    <p className="card-text">XXX XXX 1234</p>
+                    <p className="card-text">Account Balance</p>
+                    <p className="card-text">$ 5000 /-</p>
                 </div>
-                <p className="card-text">XXX XXX 1234</p>
-                <p className="card-text">Account Balance</p>
-                <p className="card-text">$ 5000 /-</p>
             </div>
         </div>
     )
