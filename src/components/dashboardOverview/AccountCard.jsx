8667680d7de485b992c9674433b332d620a12208
Abash Ansari <ansariabash2004@gmail.com>
2025-08-24 02:57:17 -0400

    added some style

--- src/components/dashboardOverview/AccountCard.jsx
+++ src/components/dashboardOverview/AccountCard.jsx
@@ -1,23 +1,33 @@
-import React from 'react'
+import React from 'react';
 
 export default function AccountCard() {
     return (
-        <div>
-            <h4 className="mb-3">Account Overview</h4>
+        <div className="container">
 
-            <div className="card w-50">
-                <div className="card-body">
-                    <div className='container d-flex justify-content-between align-items-center'>
-                        <h5 className="card-title">My Account</h5>
-                        <a href="#" className="btn btn-primary">
-                            <img src='...' alt='Bank Logo' />
-                        </a>
+            <h4 className="text-dark mb-4" style={{ fontWeight: 600 }}>Account Overview</h4>
+
+
+            <div className="row">
+                <div className="col-12 col-md-6 col-lg-4">
+
+                    <div className="card shadow-sm border-0 mb-4" style={{ backgroundColor: '#f8f9fa' }}>
+                        <div className="card-body">
+                            <div className='d-flex justify-content-between align-items-center mb-3'>
+                                <h5 className="card-title text-dark mb-0">My Account</h5>
+                                <a href="#" className="btn btn-outline-secondary p-2" style={{ borderRadius: '8px' }}>
+                                    <img src='...' alt='Bank Logo' style={{ height: '24px' }} />
+                                </a>
+                            </div>
+
+                            <p className="card-text text-muted mb-1">Account Number</p>
+                            <p className="card-text text-dark mb-3" style={{ fontWeight: 500 }}>XXX XXX 1234</p>
+
+                            <p className="card-text text-muted mb-1">Account Balance</p>
+                            <p className="card-text text-dark" style={{ fontSize: '1.25rem', fontWeight: 600 }}>$ 5,000 /-</p>
+                        </div>
                     </div>
-                    <p className="card-text">XXX XXX 1234</p>
-                    <p className="card-text">Account Balance</p>
-                    <p className="card-text">$ 5000 /-</p>
                 </div>
             </div>
         </div>
-    )
+    );
 }
