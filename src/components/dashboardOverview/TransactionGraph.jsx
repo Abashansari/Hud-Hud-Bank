53988af6e35e05ae7c93d442cdacd0a2d54b899b
Abash Ansari <ansariabash2004@gmail.com>
2025-08-24 03:16:49 -0400

    created a structure for Transaction graph

---
+++ src/components/dashboardOverview/TransactionGraph.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function TransactionGraph() {
+    return (
+        <div className='conatiner mt-5'>
+            <div class="card w-75 mb-3">
+                <div class="card-body">
+                    <h5 class="card-title">Network Summary</h5>
+                    <button className="button is-black">Total earned</button>
+                    <button className="button is-black">Total owed</button>
+                    {/* for graph .... */}
+                </div>
+            </div>
+        </div>
+    )
+}
